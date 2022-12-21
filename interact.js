747bba1fcc13713c01e0cbe49879b8697eba75c4
Akash Deep Singhal <akigupta131@gmail.com>
2022-12-21 17:31:00 +0530

    Frontend functionality for interaction created using browserify on interact.js

--- interact.js
+++ interact.js
@@ -3,35 +3,37 @@ const { abi, address } = require("./contractArtifacts");
 
 let contract = new web3.eth.Contract(JSON.parse(abi), address);
 
-// console.log(contract);
-
 async function get() {
   const num = await contract.methods.get().call();
   console.log(num);
+  return num;
 }
 
 async function set(val) {
-  const num = await contract.methods.set(val).send({
+  const transaction = await contract.methods.set(val).send({
     from: myAccount.address,
     gas: 800000,
   });
-  console.log(num);
+  console.log(transaction);
+  return transaction.transactionHash;
 }
 
-// set(6);
-get();
-
 // for interacting with the browser
-// window.addEventListener("load", () => {
-//   document.getElementById("get").onclick = () => {
-//     get().then((r) => {
-//       document.getElementById("resultOfGet").innerHTML = r;
-//     });
-//   };
-//   document.getElementById("set").onclick = () => {
-//     val = document.getElementById("value").value;
-//     set(val).then((r) => {
-//       document.getElementById("resultOfSet").innerHTML = r;
-//     });
-//   };
-// });
+window.addEventListener("load", () => {
+  const getValue = () => {
+    get().then((r) => {
+      document.getElementById("resultOfGet").innerHTML = r;
+    });
+  };
+
+  document.getElementById("get").onclick = () => {
+    getValue();
+  };
+  document.getElementById("set").onclick = () => {
+    val = document.getElementById("value").value;
+    set(val).then((r) => {
+      document.getElementById("resultOfSet").innerHTML = r;
+      getValue();
+    });
+  };
+});
